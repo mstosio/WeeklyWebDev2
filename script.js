054d89dbd23cde3a8f24e82aad05bb34c09f93ff
mstosio <michaustosio@gmail.com>
2018-03-28 00:00:23 +0200

    menu

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-var x = document.querySelector(".hamburger");
-var y = document.querySelector(".navbar_menu");
+var hamburger = document.querySelector(".hamburger_icon")
 
-x.addEventListener("click", function () {
-	if()
-});
+
+
+
+hamburger.addEventListener("click", function(){
+		var y = document.querySelector(".navbar_menu ul");
+		if (y.style.display === "none") {
+	        y.style.display = "block";
+	    } else {
+	        y.style.display = "none";
+	     }
+	});
+
+
+function myFunction(x) {
+	var y = document.querySelector(".navbar_menu ul");
+    if (x.matches) { 
+        y.style.display = "none";
+    } else {
+        y.style.display = "block";
+    }
+}
+
+var x = window.matchMedia("(max-width: 700px)")
+myFunction(x) 
+x.addListener(myFunction) 
